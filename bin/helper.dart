2ece109f37b66fed65b028692037a30a8f93e4dd
hariton1500 <hariton1500@gmail.com>
2022-03-14 09:20:30 +0300

    2

--- bin/helper.dart
+++ bin/helper.dart
@@ -2,7 +2,37 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Handle {
-  //sdgsdg
+  Future<void> getUpdates({required Telega telega}) async {
+    var res = await telega.getUpdate();
+    if (res is Map) {
+      if (res['ok']) {
+        List<dynamic> messages = res['result'];
+        print('=====================getUpdates========[${DateTime.now()}]============');
+        for (var message in messages) {
+          //print(message);
+          if (telega.updateId! < message['update_id']) {
+            print('--------------------New message or edited message----------------------');
+            print('update_id = ${message['update_id']}');
+            telega.updateId = message['update_id'];
+            dynamic mess;
+            if (message.containsKey('message')) {
+              mess = message['message'];
+            }
+            if (message.containsKey('edited_message')) {
+              mess = message['edited_message'];
+            }
+            print(mess);
+          } else {
+            print('old message... ignoring');
+            telega.updateId = message['update_id'];
+          }
+        }
+      } else {
+        //print('result is not ok');
+      }
+    }
+
+  }
 }
 
 class Telega {
@@ -13,6 +43,7 @@ class Telega {
 
   Telega({required String tkn}) {
     url = 'https://api.telegram.org/bot$tkn/';
+    updateId = 0;
   }
   Future<dynamic> getUpdate() async {
     String _url = url! + 'getUpdates';
